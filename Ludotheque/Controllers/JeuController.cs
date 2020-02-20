3b586d4623e6d425d86093e9d373726da89fed75
remy launois <remy.launois@gmail.com>
2020-02-20 16:58:57 +0100

    Premier Commit Initialisation de la structure du projet

---
+++ Ludotheque/Controllers/JeuController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ludotheque.Controllers
+{
+    public class JeuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: /HelloWorld/Jeu
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}

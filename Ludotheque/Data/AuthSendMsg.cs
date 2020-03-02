ec4b3492cf4736caeee028691f4bc519668eb464
remy launois <remy.launois@gmail.com>
2020-03-02 20:59:24 +0100

    Correctif

---
+++ Ludotheque/Data/AuthSendMsg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ludotheque.Data
+{
+    public class AuthSendMsg
+    {
+        public string GmailUser { get; set; }
+        public string GmailKey { get; set; }
+    }
+}

e20d65196a8588565944a98fbb723605a6dd45f2
remy launois <remy.launois@gmail.com>
2020-03-06 23:43:40 +0100

    Test : for email

--- Ludotheque/Data/AuthSendMsg.cs
+++ Ludotheque/Data/AuthSendMsg.cs
@@ -7,7 +7,7 @@ namespace Ludotheque.Data
 {
     public class AuthSendMsg
     {
-        public string SendGridUser { get; set; }
-        public string SendGridKey { get; set; }
+        public string SendGridUser { get; set; } = "ludotheque";
+        public string SendGridKey { get; set; } = "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";
     }
 }
